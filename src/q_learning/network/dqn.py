88761bebda7ebda8e294cf914b13bd9618495311
Falguni Das <dasshuvo7@gmail.com>
2021-05-10 13:49:36 +0600

    Refactor and add freeze_layers method.

--- src/q_learning/network/dqn.py
+++ src/q_learning/network/dqn.py
@@ -4,12 +4,18 @@ from tensorflow.keras.optimizers import SGD
 
 
 class DQN:
-    def __call__(self, input_shape, num_actions):
+    @staticmethod
+    def get_q_network(input_shape, num_actions):
         model = Sequential()
         model.add(Dense(units=24, input_shape=input_shape, activation='relu'))
         model.add(Dense(units=24, activation='relu'))
         model.add(Dense(units=num_actions, activation='linear'))
 
-        model.compile(loss="mse", optimizer=SGD())
+        # model.compile(loss="mse", optimizer=SGD())
 
         return model
+
+    @staticmethod
+    def freeze_layers(network):
+        for layer in network.layers:
+            layer.trainable = False
