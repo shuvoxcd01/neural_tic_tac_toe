8885b0cb72f96700bee14abb6b6f88487ef56f04
Falguni Das <dasshuvo7@gmail.com>
2021-05-11 22:00:15 +0600

    Add method to clone a model.

--- src/q_learning/network/dqn.py
+++ src/q_learning/network/dqn.py
@@ -1,21 +1,21 @@
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import clone_model
 
 
 class DQN:
     @staticmethod
     def get_q_network(input_shape, num_actions):
         model = Sequential()
-        model.add(Dense(units=24, input_shape=input_shape, activation='relu'))
-        model.add(Dense(units=24, activation='relu'))
-        model.add(Dense(units=num_actions, activation='linear'))
-
-        # model.compile(loss="mse", optimizer=SGD())
+        model.add(Dense(units=100, input_shape=input_shape, activation='relu'))
+        model.add(Dense(units=50, activation='relu'))
+        model.add(Dense(units=num_actions))
 
         return model
 
     @staticmethod
-    def freeze_layers(network):
-        for layer in network.layers:
-            layer.trainable = False
+    def clone(model):
+        cloned_model = clone_model(model=model)
+        cloned_model.set_weights(model.get_weights())
+
+        return cloned_model
