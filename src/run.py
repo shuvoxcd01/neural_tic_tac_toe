34fc1327dffa7fd3d2dcf400def3d3e546d7a62e
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-29 21:01:58 +0600

    Refactor.

--- src/run.py
+++ src/run.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from pettingzoo.classic import tictactoe_v3
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -10,7 +11,7 @@ from src.q_learning.agent.agent import Agent
 from src.q_learning.network.dqn import DQN
 from src.q_learning.policies.epsilon_greedy_policy import EpsilonGreedyPolicy
 from src.q_learning.policies.greedy_policy import GreedyPolicy
-from src.q_learning.q_learning import QLearning
+from src.q_learning.policies.human_policy import HumanPolicy
 from src.q_learning.transition_table.transition_table import TransitionTable
 
 env = tictactoe_v3.env()
@@ -21,19 +22,32 @@ num_actions = 9
 
 agents = {}
 
-for agent_name in env.agents:
-    q_network = DQN.get_q_network(input_shape=input_shape, num_actions=num_actions)
-    target_q_network = DQN.clone(q_network)
-    transition_table = TransitionTable()
-    behavior_policy = EpsilonGreedyPolicy(q_network=q_network)
-    target_policy = GreedyPolicy(q_network=target_q_network)
-    agent = Agent(q_network=q_network, target_q_network=target_q_network, transition_table=transition_table,
-                  behavior_policy=behavior_policy, target_policy=target_policy, agent_name=agent_name)
+q_network = DQN.get_q_network(input_shape=input_shape, num_actions=num_actions)
+target_q_network = DQN.clone(q_network)
+transition_table = TransitionTable()
+behavior_policy = EpsilonGreedyPolicy(q_network=q_network)
+target_policy = GreedyPolicy(q_network=target_q_network)
+dqn_agent = Agent(q_network=q_network, target_q_network=target_q_network, transition_table=transition_table,
+                  behavior_policy=behavior_policy, target_policy=target_policy, agent_name="DQN_Agent")
 
-    agents[agent_name] = agent
+agents[dqn_agent.name] = dqn_agent
+
+human_policy = HumanPolicy(num_actions=num_actions)
+human_agent = Agent(q_network=None, target_q_network=None, transition_table=None, behavior_policy=None,
+                    target_policy=human_policy, agent_name="Human_Agent")
+agents[human_agent.name] = human_agent
 
 env.reset()
+env.render()
+for _ in env.agent_iter():
+    for agent in agents.values():
+        observation_with_action_mask, reward, done, _ = env.last(observe=True)
 
-q_learning = QLearning(env=env, num_actions=num_actions, agents=agents)
+        observation = observation_with_action_mask["observation"]
+        action_mask = observation_with_action_mask["action_mask"]
 
-q_learning.train(3000)
+        action = agent.target_policy.get_action(tf.expand_dims(tf.identity(observation), 0),
+                                                action_mask).numpy() if not done else None
+
+        env.step(action)
+        env.render()
