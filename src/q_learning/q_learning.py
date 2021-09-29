6a869dfbf632a04bd50923654ffbb0cd8c068ae9
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-29 19:33:29 +0600

    Refactor to work with new environment.

--- src/q_learning/q_learning.py
+++ src/q_learning/q_learning.py
@@ -1,24 +1,13 @@
-import gym
-
-from src.q_learning.network.dqn import DQN
-from src.q_learning.policies.epsilon_greedy_policy import EpsilonGreedyPolicy
-from src.q_learning.policies.greedy_policy import GreedyPolicy
 import tensorflow as tf
 
-from src.q_learning.transition_table.transition_table import TransitionTable
+from src.q_learning.network.dqn import DQN
 
 
 class QLearning:
-    def __init__(self, env: gym.Env, input_shape, num_actions):
+    def __init__(self, env, num_actions, agents):
         self.env = env
         self.num_actions = num_actions
-        self.input_shape = input_shape
-        self.transition_table = TransitionTable()
-        self.q_network = DQN.get_q_network(input_shape=self.input_shape, num_actions=self.num_actions)
-        self.target_q_network = DQN.clone(self.q_network)
-
-        self.behavior_policy = EpsilonGreedyPolicy(q_network=self.q_network)
-        self.target_policy = GreedyPolicy(q_network=self.target_q_network)
+        self.agents = agents
 
         self.gamma = 0.9
         self.learning_rate = 0.01
@@ -32,36 +21,67 @@ class QLearning:
     def collect_episode(self, num_episodes):
         cur_episode_num = 0
 
-        while cur_episode_num < num_episodes:
-            observation = self.env.reset()
-            done = False
+        prev_step_info = {}
+        self.env.reset()
+        for agent_name in self.env.agents:
+            prev_step_info[agent_name] = {}
 
-            while not done:
-                action = self.behavior_policy.get_action(tf.expand_dims(tf.identity(observation), 0))
-                next_observation, reward, done, _ = self.env.step(action.numpy())
-                self.transition_table.add(s=observation, a=action, r=reward, s2=next_observation, is_term=done)
-                observation = next_observation
+        while cur_episode_num < num_episodes:
+            self.env.reset()
+
+            for _ in range(self.env.num_agents):
+                agent_name = self.env.agent_selection
+                agent = self.agents[agent_name]
+                observation_with_action_mask = self.env.observe(agent_name)
+                observation = observation_with_action_mask["observation"]
+                action_mask = observation_with_action_mask["action_mask"]
+                action = agent.behavior_policy.get_action(tf.expand_dims(tf.identity(observation), 0),
+                                                          action_mask).numpy()
+
+                prev_step_info[agent_name]["observation"] = observation
+                prev_step_info[agent_name]["action"] = action
+
+                self.env.step(action)
+
+            for agent_name in self.env.agent_iter():
+                agent = self.agents[agent_name]
+
+                prev_observation = prev_step_info[agent_name]["observation"]
+                action_taken = prev_step_info[agent_name]["action"]
+
+                observation_with_action_mask, reward, done, _ = self.env.last(observe=True)
+                observation = observation_with_action_mask["observation"]
+                action_mask = observation_with_action_mask["action_mask"]
+                agent.transition_table.add(s=prev_observation, a=action_taken, r=reward, s2=observation, is_term=done)
+
+                action = agent.behavior_policy.get_action(tf.expand_dims(tf.identity(observation), 0),
+                                                          action_mask).numpy() if not done else None
+
+                prev_step_info[agent_name]["observation"] = observation
+                prev_step_info[agent_name]["action"] = action
+
+                self.env.step(action)
 
             cur_episode_num += 1
 
     def train_one_step(self, batch_size=8):
-        with tf.GradientTape() as tape:
-            loss = self.compute_loss(batch_size)
-        self.optimizer.minimize(loss=loss, var_list=self.target_q_network.trainable_weights, tape=tape)
+        for agent in self.agents.values():
+            with tf.GradientTape() as tape:
+                loss = self.compute_loss(agent, batch_size)
+            self.optimizer.minimize(loss=loss, var_list=agent.target_q_network.trainable_weights, tape=tape)
 
-    def compute_loss(self, batch_size):
-        s, a, r, s2, term = self.transition_table.sample(size=batch_size)
-        q_value_for_s2 = tf.reduce_max(self.q_network(s2), axis=1)
+    def compute_loss(self, agent, batch_size):
+        s, a, r, s2, term = agent.transition_table.sample(size=batch_size)
+        q_value_for_s2 = tf.reduce_max(agent.q_network(s2), axis=1)
         term = (term - 1) * (-1)
-        target_q_values = r + term * q_value_for_s2 * self.gamma
-        predicted_q_values_for_all_actions = self.target_q_network(s)
+        target_q_values = tf.cast(r, dtype=tf.float32) + term * q_value_for_s2 * self.gamma
+        predicted_q_values_for_all_actions = agent.target_q_network(s)
         one_hot_actions = tf.one_hot(indices=a, depth=self.num_actions)
         predicted_q_values = tf.reduce_sum(tf.multiply(predicted_q_values_for_all_actions, one_hot_actions), axis=1)
         loss = tf.losses.MSE(y_true=target_q_values, y_pred=predicted_q_values)
         return loss
 
     def train(self, num_iterations):
-
         self.collect_episode(100)
 
         step = 0
@@ -71,13 +91,15 @@ class QLearning:
             self.train_one_step(batch_size=self.batch_size)
 
             if step % self.target_q_network_update_interval == 0:
-                self.q_network = DQN.clone(self.target_q_network)
-                print("q_network updated.")
+                for agent in self.agents.values():
+                    agent.q_network = DQN.clone(agent.target_q_network)
+                    print(f"[{agent.name}] q_network updated.")
 
             if step % self.log_interval == 0:
                 print("Step: ", step)
-                print("Loss: ", self.compute_loss(5))
-                print("epsilon: ", self.behavior_policy.epsilon)
+                for agent in self.agents.values():
+                    print(f"[{agent.name}] Loss: ", self.compute_loss(agent, 5))
+                    print(f"[{agent.name}] epsilon: ", agent.behavior_policy.epsilon)
 
             if step % self.eval_interval == 0:
                 print("Average return per episode: ", self.evaluate())
@@ -86,22 +108,37 @@ class QLearning:
 
     def evaluate(self, num_episodes=5):
         cur_episode_num = 0
-        total_return = 0
+        total_return = {}
+        for agent in self.agents.values():
+            total_return[agent.name] = 0
+
         while cur_episode_num < num_episodes:
-            observation = self.env.reset()
-            done = False
-            episode_return = 0
+            self.env.reset()
 
-            while not done:
-                observation = tf.expand_dims(input=observation, axis=0)
-                action = self.target_policy.get_action(observation)
-                observation, reward, done, info = self.env.step(action.numpy())
+            episode_return = {}
+            for agent in self.agents.values():
+                episode_return[agent.name] = 0
 
-                episode_return += reward
+            for agent_name in self.env.agent_iter():
+                agent = self.agents[agent_name]
 
-            total_return += episode_return
+                observation_with_action_mask, reward, done, _ = self.env.last(observe=True)
+                episode_return[agent_name] += reward
+                observation = observation_with_action_mask["observation"]
+                action_mask = observation_with_action_mask["action_mask"]
+
+                action = agent.target_policy.get_action(tf.expand_dims(tf.identity(observation), 0),
+                                                        action_mask).numpy() if not done else None
+
+                self.env.step(action)
+
+            for agent in self.agents.values():
+                total_return[agent.name] += episode_return[agent.name]
+
             cur_episode_num += 1
 
-        average_return_per_episode = total_return / float(num_episodes)
+        average_return_per_episode = {}
+        for agent in self.agents.values():
+            average_return_per_episode[agent.name] = total_return[agent.name] / float(num_episodes)
 
         return average_return_per_episode
