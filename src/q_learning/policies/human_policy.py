72ca24d0320df2f24cae500c13411cc43bc0c379
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-29 21:00:06 +0600

    Add human player policy.

---
+++ src/q_learning/policies/human_policy.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+
+from src.q_learning.policies.policy import Policy
+
+
+class HumanPolicy(Policy):
+    def __init__(self, num_actions: int):
+        self.num_actions = num_actions
+
+    def get_action(self, observation, action_mask):
+        actions = np.arange(self.num_actions)[action_mask.astype(bool)]
+        action = int(input(f"Select an action from {actions.tolist()}"))
+
+        return tf.constant(action)
