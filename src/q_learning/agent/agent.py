e64adbdaeb55060b874afcf3923f3ef3178ebacb
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-29 19:34:39 +0600

    Add Agent class.

---
+++ src/q_learning/agent/agent.py
@@ -0,0 +1,8 @@
+class Agent:
+    def __init__(self, q_network, target_q_network, transition_table, behavior_policy, target_policy, agent_name):
+        self.q_network = q_network
+        self.target_q_network = target_q_network
+        self.transition_table = transition_table
+        self.behavior_policy = behavior_policy
+        self.target_policy = target_policy
+        self.name = agent_name
