393d5a05e8614202b0498b820408316271c5f83f
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-29 21:11:35 +0600

    Refactor.

--- src/run.py
+++ src/run.py
@@ -1,3 +1,5 @@
+import os
+
 import tensorflow as tf
 from pettingzoo.classic import tictactoe_v3
 
@@ -9,10 +11,9 @@ from pettingzoo.classic import tictactoe_v3
 #     print("No GPU found")
 from src.q_learning.agent.agent import Agent
 from src.q_learning.network.dqn import DQN
-from src.q_learning.policies.epsilon_greedy_policy import EpsilonGreedyPolicy
 from src.q_learning.policies.greedy_policy import GreedyPolicy
 from src.q_learning.policies.human_policy import HumanPolicy
-from src.q_learning.transition_table.transition_table import TransitionTable
+from src.q_learning.saved_models import saved_model_parent_dir
 
 env = tictactoe_v3.env()
 env.reset()
@@ -23,12 +24,12 @@ num_actions = 9
 agents = {}
 
 q_network = DQN.get_q_network(input_shape=input_shape, num_actions=num_actions)
-target_q_network = DQN.clone(q_network)
-transition_table = TransitionTable()
-behavior_policy = EpsilonGreedyPolicy(q_network=q_network)
+saved_model_path = os.path.join(saved_model_parent_dir, "20210929-201050", "player_13001")
+target_q_network = tf.keras.models.load_model(saved_model_path)
+
 target_policy = GreedyPolicy(q_network=target_q_network)
-dqn_agent = Agent(q_network=q_network, target_q_network=target_q_network, transition_table=transition_table,
-                  behavior_policy=behavior_policy, target_policy=target_policy, agent_name="DQN_Agent")
+dqn_agent = Agent(q_network=None, target_q_network=target_q_network, transition_table=None,
+                  behavior_policy=None, target_policy=target_policy, agent_name="DQN_Agent")
 
 agents[dqn_agent.name] = dqn_agent
 
