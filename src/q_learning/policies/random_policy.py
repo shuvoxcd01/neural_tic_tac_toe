74b1daab1457f11f746cfae69d0bcee492a59006
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-30 21:02:58 +0600

    Add random policy.

---
+++ src/q_learning/policies/random_policy.py
@@ -0,0 +1,18 @@
+import random
+
+import numpy as np
+import tensorflow as tf
+
+from src.q_learning.policies.policy import Policy
+
+
+class RandomPolicy(Policy):
+    def __init__(self, num_actions):
+        self.num_actions = num_actions
+
+    def get_action(self, observation, action_mask):
+        binary_mask = action_mask.astype(bool)
+        valid_actions = np.arange(self.num_actions)[binary_mask]
+        action = tf.constant(random.choice(valid_actions))
+
+        return action
