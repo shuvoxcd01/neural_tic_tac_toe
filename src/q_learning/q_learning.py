d04673a8fb88903c5742d79d423d7858fb689587
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-30 21:02:46 +0600

    Refactor.

--- src/q_learning/q_learning.py
+++ src/q_learning/q_learning.py
@@ -1,34 +1,27 @@
-import os
-from datetime import datetime
+from typing import Dict
 
 import tensorflow as tf
 
-from src.q_learning.logs import tf_log_parent_dir
 from src.q_learning.network.dqn import DQN
-from src.q_learning.saved_models import saved_model_parent_dir
 
 
 class QLearning:
-    def __init__(self, env, num_actions, agents):
+    def __init__(self, env, num_actions, agents: Dict, tf_log_dir, file_writer):
         self.env = env
         self.num_actions = num_actions
         self.agents = agents
+        self.trainable_agents = [agent for agent in self.agents.values() if agent.trainable]
 
         self.gamma = 0.9
         self.learning_rate = 0.01
         self.batch_size = 64
 
         self.optimizer = tf.optimizers.SGD(learning_rate=self.learning_rate)
-        self.target_q_network_update_interval = 500
-        self.eval_interval = 2000
-        self.log_interval = 200
+        self.target_q_network_update_interval = 250
+        self.log_interval = 500
 
-        date_time_now = datetime.now().strftime("%Y%m%d-%H%M%S")
-        self.dir_to_save_models = os.path.join(saved_model_parent_dir, date_time_now)
-        self.tf_log_dir = os.path.join(tf_log_parent_dir, date_time_now)
-        self.file_writer = tf.summary.create_file_writer(logdir=self.tf_log_dir)
-
-        self.model_saving_interval = 5000
+        self.tf_log_dir = tf_log_dir
+        self.file_writer = file_writer
 
     def collect_episode(self, num_episodes):
         cur_episode_num = 0
@@ -64,7 +57,10 @@ class QLearning:
                 observation_with_action_mask, reward, done, _ = self.env.last(observe=True)
                 observation = observation_with_action_mask["observation"]
                 action_mask = observation_with_action_mask["action_mask"]
-                agent.transition_table.add(s=prev_observation, a=action_taken, r=reward, s2=observation, is_term=done)
+
+                if agent.trainable:
+                    agent.transition_table.add(s=prev_observation, a=action_taken, r=reward, s2=observation,
+                                               is_term=done)
 
                 action = agent.behavior_policy.get_action(tf.expand_dims(tf.identity(observation), 0),
                                                           action_mask).numpy() if not done else None
@@ -77,7 +73,7 @@ class QLearning:
             cur_episode_num += 1
 
     def train_one_step(self, batch_size=8):
-        for agent in self.agents.values():
+        for agent in self.trainable_agents:
             with tf.GradientTape() as tape:
                 loss = self.compute_loss(agent, batch_size)
             self.optimizer.minimize(loss=loss, var_list=agent.target_q_network.trainable_weights, tape=tape)
@@ -103,36 +99,17 @@ class QLearning:
             self.train_one_step(batch_size=self.batch_size)
 
             if step % self.target_q_network_update_interval == 0:
-                for agent in self.agents.values():
+                for agent in self.trainable_agents:
                     agent.q_network = DQN.clone(agent.target_q_network)
                     print(f"[Agent: {agent.name}] [Step: {step}] q_network updated.")
 
             if step % self.log_interval == 0:
                 with self.file_writer.as_default():
-                    for agent in self.agents.values():
+                    for agent in self.trainable_agents:
                         loss = self.compute_loss(agent, 5)
                         tf.summary.scalar(f"[{agent.name}] Loss", loss, step=step)
                         tf.summary.scalar(f"[{agent.name}] Epsilon", agent.behavior_policy.epsilon, step=step)
                         tf.summary.flush()
-
-            if step % self.eval_interval == 0:
-                avg_return_per_ep = self.evaluate()
-                with self.file_writer.as_default():
-                    for agent in self.agents.values():
-                        agent_name = agent.name
-                        tf.summary.scalar(f"[{agent_name}] Average return per episode", avg_return_per_ep[agent_name],
-                                          step=step)
-                        tf.summary.flush()
-
-            if step % self.model_saving_interval == 0:
-                for agent in self.agents.values():
-                    DQN.save_model(model=agent.target_q_network, saved_model_dir=self.dir_to_save_models,
-                                   saved_model_name=agent.name + "_" + str(step))
-                    print(f"[{agent.name}] Step {step}: Target network saved.")
-
-        for agent in self.agents.values():
-            DQN.save_model(model=agent.target_q_network, saved_model_dir=self.dir_to_save_models,
-                           saved_model_name=agent.name + "_" + str(step))
 
     def evaluate(self, num_episodes=5):
         cur_episode_num = 0
