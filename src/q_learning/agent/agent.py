53a7e6dc00cea10ca402050c8522b18a4f7989c1
shuvoxcd01 <dasshuvo7@gmail.com>
2021-09-30 21:01:48 +0600

    Add method to clone agent.

--- src/q_learning/agent/agent.py
+++ src/q_learning/agent/agent.py
@@ -1,8 +1,31 @@
+import copy
+from typing import Optional
+
+from src.q_learning.network.dqn import DQN
+
+
 class Agent:
-    def __init__(self, q_network, target_q_network, transition_table, behavior_policy, target_policy, agent_name):
+    def __init__(self, q_network, target_q_network, transition_table, behavior_policy, target_policy, agent_name,
+                 is_trainable=True):
         self.q_network = q_network
         self.target_q_network = target_q_network
         self.transition_table = transition_table
         self.behavior_policy = behavior_policy
         self.target_policy = target_policy
         self.name = agent_name
+        self.trainable = is_trainable
+
+    def clone(self, set_trainable: Optional[bool] = None):
+        cloned_agent = copy.deepcopy(self)
+        q_network = DQN.clone(self.q_network)
+        target_q_network = DQN.clone(self.target_q_network)
+
+        cloned_agent.q_network = q_network
+        cloned_agent.target_q_network = target_q_network
+        cloned_agent.behavior_policy.set_q_network(q_network=q_network)
+        cloned_agent.target_policy.set_q_network(q_network=target_q_network)
+
+        if set_trainable is not None:
+            cloned_agent.trainable = set_trainable
+
+        return cloned_agent
